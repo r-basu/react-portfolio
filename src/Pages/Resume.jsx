e58dea028a568f06ae644c8f61e09568907cd1d8
Rahul Basu <142247733+r-basu@users.noreply.github.com>
2024-02-10 11:33:47 -0500

    add resume section

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -2,6 +2,19 @@ export default function Resume() {
   return (
     <>
       <h2>Resume</h2>
+      <p>You can download my<a href="https://docs.google.com/document/d/1xF9JqO8PZyoWgqmqCF9hB6hkrXoo3QXVRztNgO9_y9Y/edit">resume here!</a></p>
+      <h3>Developer Proficiencies</h3>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+        <li>HTML</li>
+        <li>Handlebars</li>
+        <li>Express</li>
+        <li>MySQL/Sequelize</li>
+        <li>NoSQL/MongoDB, Mongoose</li>
+        <li>GraphQL</li>
+      </ul>
     </>
   );
 }
